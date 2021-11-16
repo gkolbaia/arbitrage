bba42b7e4b5fa25e825eb04518ec03b53a3ef008
gkolbaia <gkolbaia3@gmail.com>
2021-11-16 14:13:49 +0400

    s

--- src/app/modules/shared/components/case-meetings-management/case-meetings-management.component.ts
+++ src/app/modules/shared/components/case-meetings-management/case-meetings-management.component.ts
@@ -1,4 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -7,13 +13,40 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./case-meetings-management.component.scss'],
 })
 export class CaseMeetingsManagementComponent implements OnInit {
+  arbitrageMeeting: FormGroup;
+  meetingTypes = [
+    {
+      en: 'main',
+      ge: 'მთავარი',
+    },
+    {
+      en: 'regular',
+      ge: 'მორიგი',
+    },
+  ];
   constructor(
     public dialogRef: MatDialogRef<CaseMeetingsManagementComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) {}
-
-  ngOnInit(): void {}
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private _fb: FormBuilder
+  ) {
+    this.arbitrageMeeting = _fb.group({
+      date: [null, Validators.required],
+      type: [null, Validators.required],
+    });
+  }
+  get arbitrageMeetingTypeControl(): FormControl {
+    return this.arbitrageMeeting.controls.type as FormControl;
+  }
+  get arbitrageMeetingDateControl(): FormControl {
+    return this.arbitrageMeeting.controls.date as FormControl;
+  }
+  ngOnInit(): void {
+    console.log(this.data);
+  }
   close() {
     this.dialogRef.close();
+  }
+  saveMeeting() {
+    console.log(this.arbitrageMeeting.value);
   }
 }
